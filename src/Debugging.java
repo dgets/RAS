9411975c06742a6d209f5ce1d86ff155f9430545
Damon Getsman <damo.gets@gmail.com>
2018-02-17 04:49:36 -0600

    Closed #11; javadoc up to date.

--- src/Debugging.java
+++ src/Debugging.java
@@ -1,4 +1,11 @@
-
+/**
+ * Currently this only contains debugging flags for old school inline
+ * debugging.  This will probably be done away with as I attempt to migrate
+ * to unit testing.
+ * 
+ * @author Damon Getsman
+ *
+ */
 public class Debugging {
 	//debugging flags
 	public static final boolean LISTER			=	false;
--- src/Util.java
+++ src/Util.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Collection of utilities for working with MyArchive entries.
+ *  
+ * @author Damon Getsman
+ *
+ */
 public class Util {
 	public static final String[] flagExtensions = { ".tar", ".tar.gz", ".tgz", ".tar.bz2", 
 	                                                ".tar.xz", ".zip", ".arj", ".tar.Z" };
@@ -12,18 +18,43 @@ public class Util {
 	
 	private static final int decompressionFactor = 2;
 	
+	/**
+	 * Determines whether or not the system is windows.
+	 * 
+	 * @return Boolean
+	 */
 	public static Boolean runningOnDoze() {
 		return System.getProperty("os.name").startsWith("Windows");
 	}
 	
+	/**
+	 * Sets directory to be wiped upon program exit.
+	 * 
+	 * @param toastThis File
+	 * @throws IOException
+	 */
 	public static void wipeDirectory(File toastThis) throws IOException {
 		toastThis.deleteOnExit();
 	}
 	
+	/**
+	 * Wipes directory contents immediately.
+	 * 
+	 * @param toastThis File
+	 * @throws IOException
+	 */
 	public static void wipeDirectoryNow(File toastThis) throws IOException {
 		toastThis.delete();
 	}
 	
+	/**
+	 * Determines, by extension only, what entries in the contents listing are
+	 * embedded archives.  Produces a hashmap with the appropriate flag and
+	 * returns it.
+	 * 
+	 * @param contents ArrayList
+	 * @return HashMap
+	 */
 	public static HashMap<String, Boolean> findEmbedded(ArrayList<String> contents) {
 		HashMap<String, Boolean> contentFlags = new HashMap<String, Boolean>();
 		Boolean hit;
@@ -46,6 +77,15 @@ public class Util {
 		return contentFlags;
 	}
 	
+	/**
+	 * Determines whether or not there is adequate drive space remaining for
+	 * the specified archive to be decompressed in the temp directory 
+	 * specified by the applicable system property.
+	 * 
+	 * @param archive MyArchive
+	 * @param partitionPath String
+	 * @return Boolean
+	 */
 	public static Boolean adequateDriveSpace(MyArchive archive, String partitionPath) {
 		File tmpDir = new File(System.getProperty("java.io.tmpdir"));
 		
@@ -53,6 +93,12 @@ public class Util {
 			(archive.getArchiveSource().length() * decompressionFactor));
 	}
 	
+	/**
+	 * Unrolls the archives specified within the passed archive.
+	 * 
+	 * @param currentArchive MyArchive
+	 * @throws Exception
+	 */
 	public static void unrollNextArchives(MyArchive currentArchive) throws Exception {
 		//ArrayList<MyArchive> nextArchives = new ArrayList<MyArchive>();
 		
@@ -65,6 +111,12 @@ public class Util {
 		}
 	}
 	
+	/**
+	 * Cleans up (utilizing wipeDirectory()) the temp directory dingleberries
+	 * left hanging around by the specified archive.
+	 * 
+	 * @param wipeExpandedArchive MyArchive
+	 */
 	public static void cleanUpMess(MyArchive wipeExpandedArchive) {
 		
 	}
